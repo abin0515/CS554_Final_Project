2b0e45980ffcb1591e84f46ddfa423abf972ef0b
Tim Z. <zhenghanecho@gmail.com>
2025-05-08 13:10:45 -0400

    auth: fixed wrong route and duplicated package list entrires

--- likes_server/routes/index.js
+++ likes_server/routes/index.js
@@ -1,10 +1,9 @@
 //Here you will import route files and export the constructor method as shown in lecture code and worked in previous labs.
 import likesRouter from './likes.js';
-import { authenticate } from '../middleware/authenticate.js';
 
 // const router = Router();
 const constructorMethod = (app) => {
-  app.use('/likes', authenticate, likesRouter);
+  app.use('/likes', likesRouter);
 
   app.use('*', (req, res) => {
     res.status(404).send("Please check you url!");
